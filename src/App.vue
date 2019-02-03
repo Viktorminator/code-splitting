c87b60cb5ced3d862c6a24a7e366c4c9661ebdc1
unknown <viktorminator@gmail.com>
2019-02-04 01:12:05 +0200

    'Step-2'

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-    <Tooltip/>
+  <div id="app">
+    <button @click="show=true">Load tooltip</button>
+    <div v-if="show">
+      <Tooltip/>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data: () => ({ show: false }),
   components: {
-    Tooltip: () => import('@/components/Tooltip')
+    Tooltip: () => import("@/components/Tooltip")
   }
 }
 </script>
